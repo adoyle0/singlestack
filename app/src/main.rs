ebf4986d5bb1304cd43c9e82f68fb98522a81ee1
Adam Doyle <adam@doordesk.net>
2026-01-10 15:03:17 -0500

    drop csr crate

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,5 +1,5 @@
+use app::App;
 use leptos::prelude::*;
-use {{ project-name }}::App;
 
 fn main() {
     // set up logging
@@ -7,8 +7,6 @@ fn main() {
     console_error_panic_hook::set_once();
 
     mount_to_body(|| {
-        view! {
-            <App />
-        }
+        view! { <App /> }
     })
 }
